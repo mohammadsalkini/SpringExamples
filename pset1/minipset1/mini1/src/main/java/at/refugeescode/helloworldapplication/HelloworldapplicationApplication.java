65a4427bfd657ff895280791879ef372e2496a58
mohammadsalkini <eng.salkini@hotmail.com>
2018-03-22 11:07:37 +0100

    minipset1

---
+++ pset1/minipset1/mini1/src/main/java/at/refugeescode/helloworldapplication/HelloworldapplicationApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.helloworldapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldapplicationApplication.class, args);
+	}
+}
